8222c40068c8916f6cc50b2711d8c1714280f2a2
mehmetCt <mehmet@cybertekschool.com>
2022-12-15 14:59:55 -0500

    users feature

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -7,12 +7,14 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.Assert;
 
 import static io.restassured.RestAssured.*;
 
 public class ApiStepDefs {
 
     String token;
+    Response response;
 
     @Given("I logged Bookit api as a {string}")
     public void i_logged_bookit_api_as_a(String role) {
@@ -22,24 +24,34 @@ public class ApiStepDefs {
     }
     @When("I sent get request to {string} endpoint")
     public void i_sent_get_request_to_endpoint(String endpoint) {
-        Response response = given().accept(ContentType.JSON)
+         response = given().accept(ContentType.JSON)
                 .header("Authorization", token)
                 .when().get(ConfigurationReader.getProperty("base_url") + endpoint);
 
-
     }
 
-
     @Then("status code should be {int}")
-    public void status_code_should_be(Integer expectedStatusCode) {
+    public void status_code_should_be(int expectedStatusCode) {
+
+        System.out.println("response.statusCode() = " + response.statusCode());
+        Assert.assertEquals(expectedStatusCode,response.statusCode());
+
 
     }
     @Then("content type is {string}")
     public void content_type_is(String expectedContentType) {
+        System.out.println("response.contentType() = " + response.contentType());
+        Assert.assertEquals(expectedContentType,response.contentType());
 
     }
+
     @Then("role is {string}")
-    public void role_is(String role) {
+    public void role_is(String expectedRole) {
+
+        System.out.println("response.path(\"role\") = " + response.path("role"));
+        System.out.println("response.jsonPath().getString(\"role\") = " + response.jsonPath().getString("role"));
+
+        Assert.assertEquals(expectedRole,response.path("role"));
 
     }
 
