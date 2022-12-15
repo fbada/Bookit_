f2b38fcc2e2027eed631936f30d26860b01b923f
mehmetCt <mehmet@cybertekschool.com>
2022-12-15 15:40:07 -0500

    Env Setup

---
+++ src/test/java/com/bookit/step_definitions/EnvironmentStepDefs.java
@@ -0,0 +1,13 @@
+package com.bookit.step_definitions;
+
+import io.cucumber.java.en.Given;
+
+public class EnvironmentStepDefs {
+
+
+    @Given("I get related environment information")
+    public void i_get_related_environment_information() {
+
+    }
+
+}
