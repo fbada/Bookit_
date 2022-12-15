ec86e64e22312363195173e0e4dac2cced87173a
mehmetCt <mehmet@cybertekschool.com>
2022-12-15 14:46:15 -0500

    Bookit All Layers Template is Created

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,21 +1,35 @@
 package com.bookit.step_definitions;
 
+import com.bookit.utilities.BookitUtils;
+import com.bookit.utilities.ConfigurationReader;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
 
 public class ApiStepDefs {
 
-
+    String token;
 
     @Given("I logged Bookit api as a {string}")
     public void i_logged_bookit_api_as_a(String role) {
+        token = BookitUtils.generateTokenByRole(role);
+        System.out.println("token = " + token);
 
     }
     @When("I sent get request to {string} endpoint")
     public void i_sent_get_request_to_endpoint(String endpoint) {
+        Response response = given().accept(ContentType.JSON)
+                .header("Authorization", token)
+                .when().get(ConfigurationReader.getProperty("base_url") + endpoint);
+
 
     }
+
+
     @Then("status code should be {int}")
     public void status_code_should_be(Integer expectedStatusCode) {
 
